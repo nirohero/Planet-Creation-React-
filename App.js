5ce06c3970cdb5a42b0b2f5ccf736907e9d3590b
Nirojan Kalaimannan <nirokal92@gmail.com>
2025-04-03 05:06:26 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+ 
+function PlanetForm(){
+  const [name, setName] = useState('');
+  const [color, setColor] = useState('');
+  const [selected, setSelected] = useState('');
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    alert(name + color);
+  }
+  return(
+    <>
+    <div className = "PlanetBG">
+    <form onSubmit={handleSubmit}>
+      <label> Enter your planet's name:
+        <input 
+        value = {name}
+        onChange = {e => setName(e.target.value)}
+        placeHolder ="Enter planet name"
+        type = "text"
+        name = "name"
+        required
+        />
+        <br/>
+      </label>
+      <label>Enter the color of your planet:
+        <input
+        value ={color}
+        onChange = {e => setColor(e.target.value)}
+        placeHolder = "Enter color of your planet"
+         type = "text"
+        color = "color"
+        required
+        />
+        <br/>
+        </label>
+        <label>Is your planet close to the sun
+        <select value = {selected} onChange = {e=> setSelected(e.target.value)}>
+          <option value = "yes" required>Yes</option>
+          <option value = "no" required>No</option>
+        </select>
+        </label>
+
+      <input type ="submit"/>
+    </form>
+    <h2>Your planet's information will appear below:</h2>
+    <div className = "CustomPlanet">
+      <p>Planet name: {name} </p>
+      <p>Planet color: {color}</p>
+      <p>Planet close to the sun: {selected}</p>
+    </div>
+    <div className = "About">
+    <p>In this website we get to create a planet and implement rings and moons to it a well! It's inspired off a project I did back in college.</p>
+    <p>Some interesting facts about <b>space</b>: Every solar system has a source of prominence, in our case it's the Sun!</p>
+    </div>
+    <div className ="Venus">
+      <p> <h2>Venus</h2> is the hottest planet in the solar system, you would think it would be Mercury since it's the closest to the sun but in truth Venus is hotter due to it's greenhouse gas effect.</p>
+    </div>
+    <br/>
+    <div className = "Mars">
+    <p> <h2>Mars</h2> is known as the red planet. It used to contain water and many scientists beleive that it used to have life as well. It's currently our next landmark for space exploration.</p>
+    </div>
+    </div>
+    </>
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<PlanetForm/>);
+function App() {
+  <>
+  <PlanetForm/>
+  </>
+}
+export default App;
